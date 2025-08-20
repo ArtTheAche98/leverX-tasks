1a105081f3e8e97cd52dadf3e90b759ce047a2c2
Artak Matiniani <artak.matiniani@icloud.com>
2025-08-20 15:14:01 +0400

    feat(users): update user models
    Update user model logic and type annotations.

--- CourseManagementApp/users/models.py
+++ CourseManagementApp/users/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from CourseManagementApp.core.choices import UserRole
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=16, choices=UserRole.choices)
